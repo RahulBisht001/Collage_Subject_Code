904c7c9fb2c938834ceddab1c808c66aaf0255b6
RahulB <rahulbisht1012@gmail.com>
2023-12-16 09:31:16 +0530

    Update

--- Operating System/OS Lab Shubham/question1.c
+++ Operating System/OS Lab Shubham/question1.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-//system call fork() is used to create a new process so here it will print hello world two times
+// system call fork() is used to create a new process so here it will print hello world two times
 int main()
 {
     fork();
     printf("Hello World!\n");
+    fork();
     return 0;
 }
